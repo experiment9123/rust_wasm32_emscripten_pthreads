36b08f40a8c72accb2af8bea266f05579b090cd8
experiment9123 <82198196+experiment9123@users.noreply.github.com>
2022-05-20 01:13:25 +0100

    Update main.rs

--- main.rs
+++ main.rs
@@ -10,8 +10,7 @@ struct _Unwind_Exception();
 
 #[cfg(target_os="emscripten")]
 #[no_mangle]
-extern "C" fn __gxx_personality_v0   (version:isize, actions:_Unwind_Action, exceptionClass:u64,unwind_exception:*const _Unwind_Exception,context:*mut _Unwind_Context)->_Unwind_Reaso
-n_Code
+extern "C" fn __gxx_personality_v0   (version:isize, actions:_Unwind_Action, exceptionClass:u64,unwind_exception:*const _Unwind_Exception,context:*mut _Unwind_Context)->_Unwind_Reason_Code
 {
         return  0;
 }
